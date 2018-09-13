8ac8edbca95def065fb410bfff3aef61a2fc53da
Nix <yiunixon@gmail.com>
2018-09-13 11:24:32 -0700

    start fun facts app

---
+++ FunFacts/FunFacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FunFacts
+//
+//  Created by Nix on 9/13/18.
+//  Copyright © 2018 NXN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
